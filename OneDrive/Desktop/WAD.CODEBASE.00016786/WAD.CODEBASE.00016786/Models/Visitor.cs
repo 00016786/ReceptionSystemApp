d1c16c3c333e199fb58007ed23a9e02382015048
00016786 <00016786wiut@gmail.com>
2024-11-30 00:01:28 +0500

    Added visitor class

---
+++ OneDrive/Desktop/WAD.CODEBASE.00016786/WAD.CODEBASE.00016786/Models/Visitor.cs
@@ -0,0 +1,13 @@
+﻿namespace WAD.CODEBASE._00016786.Models
+{
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public Employee EmployeeID { get; set; }
+        public Room RoomID { get; set; }
+    }
+}
