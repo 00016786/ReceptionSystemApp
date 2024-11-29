0d75cffc9091b4fff3186d0b66aaae1741ca408c
00016786 <00016786wiut@gmail.com>
2024-11-29 23:59:47 +0500

    Added room class

---
+++ OneDrive/Desktop/WAD.CODEBASE.00016786/WAD.CODEBASE.00016786/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace WAD.CODEBASE._00016786.Models
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public decimal RoomPrice { get; set; }
+        public bool IsAvailable {  get; set; }
+
+    }
+}
