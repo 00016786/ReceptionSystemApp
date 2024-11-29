a619f8272c826fdf82d7cc6b50ca1abdac2f8595
00016786 <00016786wiut@gmail.com>
2024-11-29 23:45:03 +0500

    Added Employee class

---
+++ OneDrive/Desktop/WAD.CODEBASE.00016786/WAD.CODEBASE.00016786/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace WAD.CODEBASE._00016786.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Position { get; set; }
+    }
+}
