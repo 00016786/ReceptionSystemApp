dcc8c483164cbcaf1002ec03caeb7261e75aeccf
00016786 <00016786wiut@gmail.com>
2024-12-01 03:53:02 +0500

    Added DbContext class

---
+++ OneDrive/Desktop/WAD.CODEBASE.00016786/WAD.CODEBASE.00016786/Models/ReceptionDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WAD.CODEBASE._00016786.Models
+{
+    public class ReceptionDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Visitor> Visitors { get; set; }
+
+        public ReceptionDbContext(DbContextOptions<ReceptionDbContext> options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Room>()
+                .HasOne(r => r.CurrentVisitor)
+                .WithOne(v => v.Room)
+                .HasForeignKey<Visitor>(v => v.RoomID)
+                .IsRequired(false);
+
+            modelBuilder.Entity<Visitor>()
+                 .HasOne(v => v.BookedBy)
+                 .WithMany()
+                 .HasForeignKey(v => v.EmployeeID)
+                 .IsRequired(true);
+        }
+    }
+}
